513455b55e7cd52c07475f06aeefaa7af5b4e547
panic <0mt40k1z38c0d9w@gmail.com>
2024-03-03 13:58:30 +0900

    Fix format

--- chapter-2/kisuuguusuu/main.go
+++ chapter-2/kisuuguusuu/main.go
@@ -2,11 +2,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <=100; i=i+1{
-	    if a := i%2; a == 0 {
-	        fmt.Println(i,"偶数")
-		} else {
-			fmt.Println(i,"奇数")
-		}
-	}
+    for i := 0; i <=100; i=i+1{
+        if a := i%2; a == 0 {
+            fmt.Println(i,"偶数")
+        } else {
+            fmt.Println(i,"奇数")
+        }
+    }
 }
--- chapter-2/omikuji/main.go
+++ chapter-2/omikuji/main.go
@@ -7,22 +7,22 @@ import (
 )
 
 func saikoro() int {
-	t := time.Now().UnixNano()
+    t := time.Now().UnixNano()
     rand.Seed(t)
     s := rand.Intn(6) + 1
     println(s)
-	return s
+    return s
 }
 
 func main() {
-	s := saikoro()
-	if  s == 6 {
-		fmt.Println(s,"大吉")
-	} else if s == 5 || s == 4 {
-		fmt.Println(s,"中吉")
-	} else if s == 3 || s == 2 {
-		fmt.Println(s,"吉")
-	} else {
-		fmt.Println(s,"凶")
-	}
+    s := saikoro()
+    if  s == 6 {
+        fmt.Println(s,"大吉")
+    } else if s == 5 || s == 4 {
+        fmt.Println(s,"中吉")
+    } else if s == 3 || s == 2 {
+        fmt.Println(s,"吉")
+    } else {
+        fmt.Println(s,"凶")
+    }
 }
