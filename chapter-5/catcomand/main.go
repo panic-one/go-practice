0fa75b37f2978435b26562a6214f0af0886ec335
panic <0mt40k1z38c0d9w@gmail.com>
2024-03-22 19:06:47 +0900

    readLinesの実装

--- chapter-5/catcomand/main.go
+++ chapter-5/catcomand/main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
-	"flag"
 )
-var number = flag.Int("n",0, "行番号")
-var n int 
-func init() {
-	flag.IntVar(&n, "n", 0, "行番号")
-}
+
+var n = flag.Bool("n", false, "行番号をつけるかどうか")
+
 func main() {
+	flag.Parse()
 	filepath := os.Args[1:]
 	for _, a := range filepath {
 		sf, err := os.Open(a)
 		if err != nil {
-			fmt.Fprintln(os.Stdout,a)
+			fmt.Fprintln(os.Stderr, "ファイル読み込みに失敗しました:", err)
 		}
-	    defer sf.Close
+		readLines(sf)
+		defer sf.Close()
 	}
-
-if number >= 1 {
-	flag.Parse()
-	fmt.Println(int.Repeat_(*number, n))
 }
+
+func readLines(fp *os.File) {
+	scanner := bufio.NewScanner(fp)
+	// 1行ずつ読み込んで繰り返す
+	for scanner.Scan() {
+		//1行分を出力する
+		fmt.Println(scanner.Text())
+	}
+	// まとめてエラー処理をする
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "読み込みに失敗しました:", err)
+	}
 }
