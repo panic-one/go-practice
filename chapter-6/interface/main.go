1e167de7603737ad7ef5f3b552a2175b73ca4188
panic <0mt40k1z38c0d9w@gmail.com>
2024-03-25 18:12:57 +0900

    インターフェース作成

---
+++ chapter-6/interface/main.go
@@ -0,0 +1,51 @@
+package main
+
+import(
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type Person struct {
+	Name string
+	Age int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("Name: %s, Age: %d", p.Name, p.Age)
+}
+
+type Home struct {
+	Address string
+	Year int
+}
+
+func (h Home) String() string {
+	return fmt.Sprintf("Address: %s, Year: %d", h.Address, h.Year)
+}
+
+type Pet struct {
+	Name string
+	Species string
+}
+
+func (p Pet) String() string {
+	return fmt.Sprintf("Name: %s, Species: %s", p.Name, p.Species)
+}
+
+func A(s Stringer) {
+	switch a := s.(type) {
+	case Person:
+		fmt.Sprintf("Name: %s, Age: %d", p.Name, p.Age)
+    case Home:
+		fmt.Sprintf("Address: %s, Year: %d", h.Address, h.Year)
+	case Pet:
+		fmt.Sprintf("Name: %s, Species: %s", p.Name, p.Species)
+	}
+}
+
+func main() {
+	
+}
