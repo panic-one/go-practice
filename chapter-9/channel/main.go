5697000e38776b78509f2186fa74e07c84d5f86f
panic <0mt40k1z38c0d9w@gmail.com>
2024-04-11 08:56:40 +0900

    channel

--- chapter-9/channel/main.go
+++ chapter-9/channel/main.go
@@ -1,19 +1,32 @@
 package main
 
-import(
+import (
+	"bufio"
 	"fmt"
-	"time"
+	"io"
+	"os"
 )
 
+func input(r io.Reader) <-chan string {
+	// TODO: チャネルを作る
+	ch1 := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			// TODO: チャネルに読み込んだ文字列を送る
+			ch1 <- s.Text()
+		}
+		// TODO: チャネルを閉じる
+		defer close(ch1)
+	}()
+	// TODO: チャネルを返す
+	return ch1
+}
+
 func main() {
-	defer fmt.Println("main done")
-	go func() {
-		defer fmt.Println("goroutine1 done")
-		time.Sleep(3 * time.Second)
-	}()
-	go func() {
-		defer fmt.Println("goroutine2 done")
-		time.Sleep(1 * time.Second)
-	}()
-	time.Sleep(5 * time.Second)
+	ch := input(os.Stdin)
+	for {
+		fmt.Print(">")
+		fmt.Println(<-ch)
+	}
 }
